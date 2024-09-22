4e439ab3e7feb880a9b389b1e3481bb210f9fe07
victorb <victorjuda49@gmail.com>
2024-09-22 19:16:06 -0300

    Adc novos métodos, implementando os cálculos

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,62 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String nome = "Josefino Alcantara";
         String tipoConta = "Corrente";
-        double saldoConta = 1230.00;
+        double saldo = 1250.00;
+        int opcao = 0;
 
-        System.out.println("********************");
-        System.out.println("Usuario: " + nome);
-        System.out.println("Tipo de conta: " + tipoConta);
-        System.out.println("Saldo em conta: " + saldoConta);
-        System.out.println("********************");
+        System.out.println("\n***********************");
+        System.out.println("Nome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("***********************");
+
+        String menu = """
+                \n --- Digite sua opção ---
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor 
+                4 - Sair
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+            switch (opcao) {
+                case 1 -> System.out.println("O saldo atualizado é " + saldo);
+                case 2 -> saldo = transferirValor(saldo, leitura);
+                case 3 -> saldo = receberValor(saldo, leitura);
+                case 4 -> System.out.println("Saindo do sistema...");
+                default -> System.out.println("Opção inválida!");
+            }
+        }
+        leitura.close();
+    }
 
 
+    public static double transferirValor(double saldo, Scanner leitura) {
+        System.out.println("Qual o valor que deseja transferir?");
+        double valor = leitura.nextDouble();
+        if (valor > saldo) {
+            System.out.println("Não há saldo para realizar a transferência.");
+        } else {
+            saldo -= valor;
+            System.out.println("Novo saldo: " + saldo);
+        }
+        return saldo;
+    }
 
+
+    public static double receberValor(double saldo, Scanner leitura) {
+        System.out.println("Valor recebido: ");
+        double valor = leitura.nextDouble();
+        saldo += valor;
+        System.out.println("Novo saldo: " + saldo);
+        return saldo;
     }
 }
