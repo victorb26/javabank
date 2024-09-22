13452ce65105f6fb864c4d880e29af1903139cc5
victorb <victorjuda49@gmail.com>
2024-09-22 18:47:51 -0300

    SECO

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Josefino Alcantara";
+        String tipoConta = "Corrente";
+        double saldoConta = 1230.00;
+
+        System.out.println("********************");
+        System.out.println("Usuario: " + nome);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo em conta: " + saldoConta);
+        System.out.println("********************");
+
+
+
+    }
+}
